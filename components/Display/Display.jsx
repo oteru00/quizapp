580a1e2bb53500175ce3051c6c83a240c7c3857f
otemizu <lyushoushui@gmail.com>
2025-09-25 23:46:13 +0900

    ファイル変更

--- components/Display/Display.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Display.module.css"
-
-export default function Display({ children }){
-    return (
-        <div className={styles.display}>
-            {children}
-        </div>
-    )
-}
