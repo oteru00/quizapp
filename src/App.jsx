4355450aa79427f833e32f480dfdd3d98f0d167e
otemizu <lyushoushui@gmail.com>
2025-09-25 23:45:51 +0900

    ファイル変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { ROUTES } from "./const";
-import QuizPage from './pages/QuizPage'
-import ResultPage from './pages/ResultPage'
-import HomePage from './pages/HomePage'
+import { useState } from 'react'
+React
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
+
+  const handleClick = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+    else {
+      setMessage("これ以上は減らせません！！")
+    }
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={ROUTES.HOME} element={<HomePage />} />
-        <Route path={ROUTES.QUIZ} element={<QuizPage />} />
-        <Route path={ROUTES.RESULT} element={<ResultPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>カウントアプリ</h1>
+      <p>現在のカウント：{count}</p>
+      <button onClick={() => { setCount(count + 1); setMessage(""); }}>+1</button>
+      <button onClick={handleClick}>-1</button>
+      <button onClick={() => { setCount(0); setMessage(""); }}>reset</button>
+
+      {message && <p role='alert' style={{color:"red"}}>{message}</p>}
+    </>
   )
 }
+
+export default App
